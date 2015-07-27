d414b3675a1ea5e6c4ab1a90d7643692cbf5647a
Anonim47 <yosefadis@hotmail.com>
2015-07-27 21:33:20 +0700

    Partially fix sound bug
    Signed-off-by: Anonim47 <yosefadis@hotmail.com>

--- cm.mk
+++ cm.mk
@@ -12,6 +12,7 @@ PRODUCT_NAME := cm_armani
 $(call inherit-product, device/lenovo/armani/full_armani.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="Lenovo/armani_row/armani_row:4.1.2/JZO54K/A706_ROW_S123_140116:user/dev-keys" \
     PRODUCT_BRAND=LENOVO \
     PRODUCT_NAME=armani \
     BUILD_PRODUCT=A706_ROW

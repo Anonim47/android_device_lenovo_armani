23c434bb788fe8d3d6ec892b3fd590cf4b383f5e
Anonim47 <yosef@yosefadi.tk>
2015-10-18 11:02:00 +0700

    armani_row: Fix up path
    * Changes armani to armani_row
    Signed-off-by: Anonim47 <yosef@yosefadi.tk>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sh device/lenovo/armani/patches/apply.sh
-sh device/lenovo/armani/manifest/apply.sh
+sh device/lenovo/armani_row/patches/apply.sh
+sh device/lenovo/armani_row/manifest/apply.sh
 
